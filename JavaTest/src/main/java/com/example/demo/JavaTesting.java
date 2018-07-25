31c136116876451ac27bc9bd4d86dd3e4bf95414
sekine3314 <gp0211@outlook.jp>
2018-07-25 12:10:52 +0900

    Java練習

--- JavaTest/src/main/java/com/example/demo/JavaTesting.java
+++ JavaTest/src/main/java/com/example/demo/JavaTesting.java
@@ -1,16 +1,25 @@
 package com.example.demo;
+import java.io.File;
 import java.io.IOException;
+//import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class JavaTesting{
     public static void main(String[] args) {
         try {
-            //FileWriter fw = new FileWriter("test.txt"); //ファイルを開く
-        	PrintWriter pw = new PrintWriter("test.txt");//改行
+            File file = new File("test.txt");
+
+            /*FileWriter fw = new FileWriter(file); //ファイルを開く
+        	fw.write("こんにちは");
+        	fw.write("こんにちは");
+        	fw.close();*/
+
+            PrintWriter pw = new PrintWriter(file);//改行
             pw.println("1");			//ファイルに書き込む
             pw.println("sekine");
             pw.println("こんにちは");
-            pw.close();			//ファイルを閉じる
+            pw.close();			//ファイルを閉じる*/
+
         } catch (IOException ex) {		//ファイルがなかった時の為の処理
             ex.printStackTrace();
         }
