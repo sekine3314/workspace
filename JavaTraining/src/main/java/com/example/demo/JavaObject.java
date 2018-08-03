a1f2ff924aa5e8f947fe8d2f8366c30c3bd409a4
sekine3314 <gp0211@outlook.jp>
2018-08-03 15:53:53 +0900

    JavaTraining

--- JavaTraining/src/main/java/com/example/demo/JavaObject.java
+++ JavaTraining/src/main/java/com/example/demo/JavaObject.java
@@ -5,6 +5,8 @@ public class JavaObject {
 	public int age;
 	public double height;
 	public double weight;
+	public int distance = 0;
+	public int limit = 100;
 
 	JavaObject(String name, int age, double height, double weight) { //コンストラクタ//
 		this.name = name;
@@ -13,8 +15,9 @@ public class JavaObject {
 		this.weight = weight;
 	}
 
-	public void hello() { //インスタンスメソッド
+	public void hello() {			 //インスタンスメソッド
 		System.out.println("こんにちは" + "\n" + "元気ですか");
+		System.out.println("-------------------------------------------------");
 	}
 
 	public void printData() {
@@ -22,5 +25,15 @@ public class JavaObject {
 		System.out.println("年齢は" + this.age + "歳です");
 		System.out.println("身長は" + this.height + "です");
 		System.out.println("体重は" + this.weight + "です");
+	}
+
+	public void run(int distance) {		 //キーボードからの入力をメソッドで受け取る。
+		if (distance >= this.limit) {		 //条件分岐の練習
+			System.out.println("無謀です。移動するのはやめておきましょう");
+		} else {
+			this.distance = distance;
+			System.out.println("運動するのはいいことです。移動しましょう！");
+		}
+		System.out.println("移動距離は" + this.distance + "kmです");
 	}
 }
--- JavaTraining/src/main/java/com/example/demo/JavaTraining.java
+++ JavaTraining/src/main/java/com/example/demo/JavaTraining.java
@@ -6,14 +6,16 @@ public class JavaTraining {
 	public static void main(String[] args) {
 
 		//オブジェクト指向でのhelloメソッド等の呼び出し//
-		JavaObject object = new JavaObject("shin", 26, 175.0, 75.0);
-		Scanner scanner = new Scanner(System.in);      //キーボード入力・表示
-		System.out.print("名前：");
-		String name = scanner.next();
-		System.out.println("名前は" + name + "です");
+		JavaObject object = new JavaObject("Java", 26, 175.0, 75.0); //インスタンスの生成
 
 		object.hello();
 		object.printData(); //個人データをまとめたメソッドを呼び出す。//
 
+		System.out.println("-------------------------------------------------");
+		Scanner scanner = new Scanner(System.in);      //キーボード入力・表示
+		System.out.print("移動する距離を入力してください(km)：");
+		int i = scanner.nextInt();
+		object.run(i);							//キーボードからの入力を引数にする
+		scanner.close();
 	}
 }
